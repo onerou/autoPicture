90b1a81f52c9bdcd6f77b9b687d1e3ea1490416c
name <1057674027@qq.com>
2020-11-21 23:25:21 +0800

    增加log4js输出

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-const express = require("express")
-const utils = require("./utils")
-const config = require("./config")
-const task = require("./task")
-const WebSocket = require("ws") // 引入模块
-const { parseTime } = require("./utils")
-const ws = new WebSocket.Server({ port: 3000 }, () => {
+const express = require("express");
+const log4js = require("log4js");
+const utils = require("./utils");
+const config = require("./config");
+const task = require("./task");
+const WebSocket = require("ws"); // 引入模块
+const { parseTime } = require("./utils");
+const ws = new WebSocket.Server({ port: 3030 }, () => {
   // 监听接口
-  console.log("socket start")
-})
-global.clients = []
-ws.on("connection", client => {
+  console.log("socket start");
+});
+log4js.configure({
+  appenders: { globalError: { type: "file", filename: "./log/error.log" } },
+  // 只有错误时error级别才会写入文件
+  categories: { default: { appenders: ["globalError"], level: "error" } },
+});
+const logger = log4js.getLogger("cheese");
+
+global.clients = [];
+ws.on("connection", (client) => {
   //console.log("client:",client)
-  global.clients.push(client)
-  client.on("message", msg => {
-    console.log("来自服务器的数据", msg)
+  global.clients.push(client);
+  client.on("message", (msg) => {
+    console.log("来自服务器的数据", msg);
     client.send(
       JSON.stringify({
         userName: "心肝大宝贝",
@@ -22,30 +30,37 @@ ws.on("connection", client => {
         autograph: "生如将逝，爱因本心",
         time: parseTime(new Date()),
         isLogin: true,
-        type: "love"
+        type: "love",
       })
-    ) // 通过send方法来给前端发送消息
+    ); // 通过send方法来给前端发送消息
     //sendall();
-  })
-  client.on("close", msg => {
-    console.log("关闭服务器连接")
-  })
-})
-const app = express()
-app.use(express.static("views"))
-app.set("view engine", "pug")
+  });
+  client.on("close", (msg) => {
+    console.log("关闭服务器连接");
+  });
+});
+const app = express();
+app.use(express.static("views"));
+app.set("view engine", "pug");
+app.use(async (ctx, next) => {
+  try {
+    await next();
+  } catch (e) {
+    logger.error(e);
+  }
+});
 
 app.get("/temp", (req, res) => {
-  const formatedDate = utils.getDate()
-  const days = utils.getDay(config.MEET_DAY)
-  const date = `${formatedDate} | 相遇的第${days}天`
+  const formatedDate = utils.getDate();
+  const days = utils.getDay(config.MEET_DAY);
+  const date = `${formatedDate} | 相遇的第${days}天`;
   res.render("template", {
     ...global.tempData,
-    date
-  })
-})
+    date,
+  });
+});
 
-app.listen(4000, async () => {
-  console.log("Example app listening on port 4000!")
-  task()
-})
+app.listen(4040, async () => {
+  console.log("Example app listening on port 4000!");
+  task();
+});
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-	apps: [
-		{
-			name: 'weChartBot',
-			script: 'app.js',
-			// Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-			instances: 1,
-			autorestart: false,
-			watch: false,
-			max_memory_restart: '4G',
-			env: {
-				PM2_SERVE_PORT: 8080,
-				PM2_SERVE_PATH: './apidoc',
-				NODE_ENV: 'production'
-			},
-			env_production: {
-				NODE_ENV: 'production'
-			}
-		}
-	]
-}
+  apps: [
+    {
+      name: "weChartBot",
+      script: "app.js",
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      instances: 1,
+      autorestart: false,
+      watch: false,
+      max_memory_restart: "4G",
+      env: {
+        PM2_SERVE_PORT: 8089,
+        PM2_SERVE_PATH: "./apidoc",
+        NODE_ENV: "production",
+      },
+      env_production: {
+        NODE_ENV: "production",
+      },
+    },
+  ],
+};
